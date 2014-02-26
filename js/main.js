484685d314b112a175c26bf60e4b5be62af79601
Andrew Lai <lai61616@gmail.com>
2014-02-25 22:12:00 -0800

    Partially handle basic question types: - Does it - Is it - Can it

--- js/main.js
+++ js/main.js
@@ -1,18 +1,69 @@
 $(function(){
   $('#submit_btn').click(function(){
-    //alert($('#q_text').val());
-    var query = 'http://jsonp.jit.su/?url=http://demo.ark.cs.cmu.edu/parse/api/v1/parse?sentence=';
-    query += encodeURI($('#q_text').val());
+    
+    var query_base = 'http://jsonp.jit.su/?url=http://demo.ark.cs.cmu.edu/parse/api/v1/parse?sentence=';
+    var query = $('#q_text').val();
+
+    query = query.replace(/^\s*is\s+it/i, 'it is');  // Is it -> It is
+
+    query = query_base + encodeURI(query);
     
     $.getJSON(query, function(data){
-      //alert('fake AJAX! ' + data);
       console.log(data.sentences);
       var tokens = data.sentences[0].tokens;
+      var entities = data.sentences[0].entities;
+      var frames = data.sentences[0].frames;
+      var relations = data.sentences[0].relations;
+      var assertion = {};
+      
       $('#results').html('');
-      for(var t in tokens) {
-        var li = '<li>' + tokens[t] + '</li>';
-        $('#results').append(li);
+
+      var idx = 0;
+      var frame_found = false;
+      // Find the first Verb 
+      for (var e in entities) {
+        if(entities[e][1] == 'VB'){ 
+          frame_found = true;
+          break;
+        }
+        idx += 1;
       }
+
+      if(frame_found) {
+        for(var f in frames) {
+          if(frames[f].target.start == idx) {
+            // Get the Frame name
+            assertion.frame_name = frames[f].target.name;
+
+            // Get the Object
+            if(frames[f].annotationSets[0].frameElements.length > 1) {  // Object in the frame 
+              assertion.object = frames[f].annotationSets[0].frameElements[1].text;
+            }
+            else {  
+              // Look for direct object
+              for(var r in relations) {
+                if(relations[r][1] == 'dobj') {
+                  var term_idx = Number(relations[r][2][1][1].substr(1)) - 1;
+                  assertion.object = tokens[term_idx];
+                }
+              }
+            }
+          }
+        }  
+      } else {  // no usable frames
+        for(var r in relations) {
+          if(relations[r][1] == 'nsubj') {
+            var term_idx = Number(relations[r][2][0][1].substr(1)) - 1;
+            if(entities[term_idx][1] == 'JJ')
+              assertion.relation = 'hasProperty';
+            else
+              assertion.relation = 'isA';
+            assertion.object = tokens[term_idx];
+          }
+        }
+      }
+
+      console.log(assertion);
     });
   });
 });
